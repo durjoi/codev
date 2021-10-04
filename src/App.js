292ae4c928c962cdc82d5793a664e7071c00edf2
Mehedi Hassan Durjoi <m.h.durjoi@gmail.com>
2021-10-04 16:30:18 +0600

    React router added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button, Col, Row } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-      <Row className="mx-0">
-        <Button as={Col} variant="primary">Button #1</Button>
-        <Button as={Col} variant="secondary" className="mx-2">Button #2</Button>
-        <Button as={Col} variant="success">Button #3</Button>
-      </Row>
+      
     </div>
   );
 }
