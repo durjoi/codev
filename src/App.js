3b8c425cb398c60316c8484ea434310c44180e5d
Mehedi Hassan Durjoi <m.h.durjoi@gmail.com>
2021-10-04 16:27:12 +0600

    Initial commit & React bootstrap added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, Col, Row } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Row className="mx-0">
+        <Button as={Col} variant="primary">Button #1</Button>
+        <Button as={Col} variant="secondary" className="mx-2">Button #2</Button>
+        <Button as={Col} variant="success">Button #3</Button>
+      </Row>
     </div>
   );
 }
