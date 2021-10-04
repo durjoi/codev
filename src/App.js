c09d674b014b35c4672531a73928cc044c937e11
Mehedi Hassan Durjoi <m.h.durjoi@gmail.com>
2021-10-04 16:42:39 +0600

    Folder structure and initial setup added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import Home from './components/home/Home';
+import Course from './components/course/Course';
+import About from './components/about/About';
+import Contact from './components/contact/Contact';
+import NotFound from './components/notFound/NotFound';
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+
+        <Header></Header>
+
+        <Switch>
+            <Route exact path='/'>
+              <Home></Home>
+            </Route>
+            <Route exact path='/course'>
+              <Course></Course>
+            </Route>
+            <Route exact path='/about'>
+              <About></About>
+            </Route>
+            <Route exact path='/contact'>
+              <Contact></Contact>
+            </Route>
+            <Route path='*'>
+              <NotFound></NotFound>
+            </Route>
+        </Switch>
+
+        <Footer></Footer>
+      </Router>
     </div>
   );
 }
---
+++ src/components/course/Course.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Course = () => {
+    return (
+        <div>
+            <h1>This is Course</h1>
+        </div>
+    );
+};
+
+export default Course;
