6ca72e56381903d533f06ba88e1b6ba220df4d5e
Mehedi Hassan Durjoi <m.h.durjoi@gmail.com>
2021-10-04 16:53:50 +0600

    Header added

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link, NavLink } from 'react-router-dom';
+import './Header.css';
 
 const Header = () => {
     return (
-        <div>
-            <h1>This is header</h1>
-        </div>
+        <header>
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Container>
+                <Navbar.Brand>
+                    <NavLink to="/home">{'{'}Codev{'}'}</NavLink>    
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="me-auto">
+                        <NavLink to="/home">Home</NavLink>
+                        <NavLink to="/course">Course</NavLink>
+                        <NavLink to="/about">About Us</NavLink>
+                    </Nav>
+                </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </header>
     );
 };
 
